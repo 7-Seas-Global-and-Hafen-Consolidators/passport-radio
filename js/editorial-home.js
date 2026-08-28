9bfbd8cf06aae33cc84d91f89b49b6197d99e42f
HafenConsol <info.hafen@proton.me>
2026-08-28 11:45:49 -0300

    Passport: load site polish after home modules

--- js/editorial-home.js
+++ js/editorial-home.js
@@ -24,5 +24,14 @@
   }
 
   const load = src => new Promise((ok,fail)=>{const s=document.createElement('script');s.src=src;s.async=false;s.onload=ok;s.onerror=fail;document.head.appendChild(s)});
-  (async()=>{try{await load('/js/global-signals-lib.js?v=5');await Promise.all([load('/js/global-signals-home.js?v=5'),load('/js/home-support.js?v=5')])}catch(e){console.error('Passport Home Wire',e)}})();
+  (async()=>{
+    try{
+      await load('/js/global-signals-lib.js?v=5');
+      await Promise.all([
+        load('/js/global-signals-home.js?v=5'),
+        load('/js/home-support.js?v=5')
+      ]);
+      await load('/js/passport-site-polish.js?v=20260828');
+    }catch(e){console.error('Passport Home Wire',e)}
+  })();
 })();
