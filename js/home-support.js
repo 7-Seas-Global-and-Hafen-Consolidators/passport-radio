0e845e3b8f74f8e58289073196966f79aed2576f
HafenConsol <info.hafen@proton.me>
2026-09-01 12:25:41 -0300

    Bust Passport support loader cache

--- js/home-support.js
+++ js/home-support.js
@@ -1,7 +1,7 @@
 (() => {
   if(document.querySelector('script[data-passport-support-global]')) return;
   const script=document.createElement('script');
-  script.src='/js/passport-support.js?v=1';
+  script.src='/js/passport-support.js?v=3';
   script.defer=true;
   script.dataset.passportSupportGlobal='1';
   document.head.appendChild(script);
