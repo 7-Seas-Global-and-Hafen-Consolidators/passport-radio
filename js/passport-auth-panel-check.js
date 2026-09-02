e1ec2f1fe7455acfc96cc2c316b3b7c839716778
HafenConsol <info.hafen@proton.me>
2026-09-02 18:15:13 -0300

    Add Passport member panel smoke check

---
+++ js/passport-auth-panel-check.js
@@ -0,0 +1,1 @@
+(()=>{'use strict';window.PassportPanelCheck={version:1,required:['account-favorites','account-votes','remember-me'],ok(){return this.required.every(id=>document.getElementById(id));}};})();
